96bca48237bb8edbb2ee9ee390d4335bd9376a3d
Vidhi Agrawal <163636860+vidhiagrawa@users.noreply.github.com>
2024-05-19 03:53:28 -0700

    Add files via upload

---
+++ Loop Que/_02.js
@@ -0,0 +1,24 @@
+// <!-- ttake a number n as an input from user create an arrays of a number from 1-n .... use the reduce method to calculate the "sum and product" of all numbers in array -->
+
+
+let n = prompt("enter a number")
+let arr = []
+
+for(let i = 1; i <= n; i++){
+    arr[i - 1] = i
+}
+
+console.log(arr)
+
+let add = arr.reduce( (sum, curr) => {
+    return sum + curr;
+} )
+
+let prod = arr.reduce( (sum, curr) => {
+    return sum * curr;
+} )
+
+console.log("the sum : ", add)
+
+console.log("the product : ", prod)
+
